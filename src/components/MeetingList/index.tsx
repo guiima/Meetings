6e1f6502092e672f2a9604da1ebef2b833022e6a
Rafael Guimarães <rafael_guiimaraes@hotmail.com>
2021-01-08 17:23:47 -0300

    removed the console.log

--- src/components/MeetingList/index.tsx
+++ src/components/MeetingList/index.tsx
@@ -34,7 +34,6 @@ const MeetingList: React.FC<MeetingListProps> = ({navigation}) => {
         if (response) {
           setMeetings(response);
         }
-        console.log('resp', response);
       })
       .catch((err) => {
         setMessage('Não foi possível carregar a lista de reuniões!');
@@ -84,7 +83,6 @@ const MeetingList: React.FC<MeetingListProps> = ({navigation}) => {
           action={() => navigation.navigate('MeetingForm')}
         />
       </Top>
-      {console.log('meetings', meetings)}
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
         {meetings.map((item) => {
           return (
